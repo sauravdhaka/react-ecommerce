f1e9d8a7da65d4e89f5fcec971ab0c7b68517cfe
SAURAV DHAKA <sauravdhaka456@gmail.com>
2023-08-23 22:45:45 +0530

    first commit login signup

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import  Counter  from './features/counter/Counter';
+import './App.css';
+import ProductList from './features/product-list/ProductList';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home></Home>),
+  },
+  {
+    path: "/login",
+    element: (<LoginPage></LoginPage>),
+  },
+  {
+    path: "/signup",
+    element: (<SignupPage></SignupPage>),
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+          <RouterProvider router={router} />
+
+    </div>
+  );
+}
+
+export default App;
